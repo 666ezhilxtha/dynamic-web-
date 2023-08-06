c5d74a1eff4a6da3ece9779c2886be6b86de2a62
lokchan sunuwar <92963591+666ezhilxtha@users.noreply.github.com>
2023-08-06 18:58:35 +0545

    Update add.php

--- website_login/add.php
+++ website_login/add.php
@@ -20,37 +20,6 @@
 </form>
 
     
-    <!-- <form action="add.php" method="POST">
-        Name:  <input type="text" name="name" id="name" value="<?php if (isset($_POST['name'])) echo $_POST ['name'];?>"> <br>
-        <br>
-        <p style="color:red">
-        <?php
-        if (isset($errors['name'])) echo $errors ['name'];
-        ?>
-    </p>
-        email: <input type="email" name="email" id="email" value="<?php if (isset($_POST['email'])) echo $_POST ['email'];?>"><br>
-        <p style="color:red">
-        <?php
-        if (isset($errors['email'])) echo $errors ['email'];
-        ?>
-
-    </p>
-
-    password: <input type="passwordt" name="password" id="password" value="<?php if (isset($_POST['password'])) echo $_POST ['password'];?>"><br>
-        <p style="color:red">
-        <?php
-        if (isset($errors['password'])) echo $errors ['password'];
-        ?>
-        </p>
-        <br>
-        age: <input type="number" name="age" value="<?php if (isset($_POST['age'])) echo $_POST ['age'];?>"><br>
-        <p style="color:red">
-        <?php
-        if (isset($errors['age'])) echo $errors ['age'];
-        ?>
-    </p>
-
-        <button>submit</button>
-    </form> -->
+ 
 </body>
 </html>
